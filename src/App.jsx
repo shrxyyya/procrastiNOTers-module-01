9555ef8129119b11ddf08c5760c89518535e0cb2
shrxyyya <shreya5.mittal3@gmail.com>
2024-06-03 20:15:12 +0530

    compressed glb

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import './App.css';
 import { OrbitControls } from '@react-three/drei';
@@ -6,14 +6,21 @@ import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useRef } from 'react';
 import { Stats } from '@react-three/drei'
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 // Component to load the GLB file
 const Model = () => {
   const glb = useLoader(GLTFLoader, '/model_imgs/mikudayo.glb');
   const ref = useRef();
 
+  const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/')
+  loader.setDRACOLoader(dracoLoader)
+
   return <primitive ref={ref} object={glb.scene} scale={[3, 3, 3]} />;
 };
+
 
 export default function App() {
   return (
