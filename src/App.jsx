1018d3d2ed1ba037c6adc95e9ff2c52021e1abaa
shrxyyya <shreya5.mittal3@gmail.com>
2024-06-03 19:50:26 +0530

    glb img, stats added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import './App.css';
+import { OrbitControls } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useRef } from 'react';
+import { Stats } from '@react-three/drei'
+
+// Component to load the GLB file
+const Model = () => {
+  const glb = useLoader(GLTFLoader, '/model_imgs/mikudayo.glb');
+  const ref = useRef();
+
+  return <primitive ref={ref} object={glb.scene} scale={[3, 3, 3]} />;
+};
+
+export default function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [-8, 3, 8] }}>
+        <ambientLight intensity={2.5} />
+        {/* <directionalLight color="white" position={[0, 0, 5]} /> */}
+        <Model />
+        <OrbitControls />
+        <Stats />
+      </Canvas>
+    </>
+  );
+}
